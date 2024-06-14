97006a2f363014d6ee55fb9ac00702419e6fa20f
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-06-14 19:29:57 +0100

    Update code for firebase setting

---
+++ geoscanner/lib/nav_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  MapBoxNavigationViewController? _controller;
+  String? _instruction;
+  final bool _isMultipleStop = false;
+  double? _distanceRemaining, _durationRemaining;
+  bool _routeBuilt = false;
+  bool _isNavigating = false;
+  bool _arrived = false;
+  late MapBoxOptions _navigationOption;
+
+  Future<void> initialize() async {
+    if (!mounted) return;
+    _navigationOption = MapBoxNavigation.instance.getDefaultOptions();
+    _navigationOption.initialLatitude = 37.7749;
+    _navigationOption.initialLongitude = -122.4194;
+    _navigationOption.mode = MapBoxNavigationMode.walking;
+    MapBoxNavigation.instance.registerRouteEventListener(_onRouteEvent);
+  }
+
+
+  @override
+  void initState() {
+    initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 1,
+            child: Container(
+              color: Colors.grey[100],
+              child: MapBoxNavigationView(
+                options: _navigationOption,
+                onRouteEvent: _onRouteEvent,
+                onCreated: (MapBoxNavigationViewController controller) async {
+                  _controller = controller;
+                  controller.initialize();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _onRouteEvent(e) async {
+
+    _distanceRemaining = await MapBoxNavigation.instance.getDistanceRemaining();
+    _durationRemaining = await MapBoxNavigation.instance.getDurationRemaining();
+
+    switch (e.eventType) {
+      case MapBoxEvent.progress_change:
+        var progressEvent = e.data as RouteProgressEvent;
+        _arrived = progressEvent.arrived!;
+        if (progressEvent.currentStepInstruction != null) {
+          _instruction = progressEvent.currentStepInstruction;
+        }
+        break;
+      case MapBoxEvent.route_building:
+      case MapBoxEvent.route_built:
+        _routeBuilt = true;
+        break;
+      case MapBoxEvent.route_build_failed:
+        _routeBuilt = false;
+        break;
+      case MapBoxEvent.navigation_running:
+        _isNavigating = true;
+        break;
+      case MapBoxEvent.on_arrival:
+        _arrived = true;
+        if (!_isMultipleStop) {
+          await Future.delayed(const Duration(seconds: 3));
+          await _controller?.finishNavigation();
+        } else {}
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        _routeBuilt = false;
+        _isNavigating = false;
+        break;
+      default:
+        break;
+    }
+    //refresh UI
+    setState(() {});
+  }
+}
